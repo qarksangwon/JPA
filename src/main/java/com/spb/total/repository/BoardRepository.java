3a7ee1ef7b36878b8ddbb6d5ef2b07bbb03fc837
qarksangdol <0629psw@gmail.com>
2024-06-04 15:26:41 +0900

    게시글레파지토리 JUnit 테스트 3가지

--- src/main/java/com/spb/total/repository/BoardRepository.java
+++ src/main/java/com/spb/total/repository/BoardRepository.java
@@ -5,10 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
-    List<Board> findByTitleContaining(String keyword);
+    Optional<Board> findByTitleContaining(String keyword);
+    List<Board> findByContentContaining(String keyword);
+    List<Board> findAll();
 }
 
 // JUnit 테스트 목록
--- src/test/java/com/spb/total/repository/BoardRepositoryTest.java
+++ src/test/java/com/spb/total/repository/BoardRepositoryTest.java
@@ -1,8 +1,16 @@
 package com.spb.total.repository;
 
+import com.spb.total.entity.Board;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,5 +18,49 @@ import static org.junit.jupiter.api.Assertions.*;
 @TestPropertySource(locations = "classpath:application-test.properties")
 @Slf4j
 class BoardRepositoryTest {
+    @Autowired
+    BoardRepository boardRepository;
+
+    @Test
+    @DisplayName("게시글 테스트 저장")
+    public void createBoardTest(){
+        for(int i = 1; i <= 5; i++){
+            Board board = new Board();
+            board.setTitle("게시글"+i);
+            board.setContent("게시글 내용" + i);
+            board.setImgPath("http://board"+i+".com");
+            board.setRegDate(LocalDateTime.now());
+            boardRepository.save(board);
+        }
+    }
+
+    @Test
+    @DisplayName("게시글 전체 조회")
+    public void findAllTest(){
+        this.createBoardTest();
+        List<Board> boardList = boardRepository.findAll();
+        for(Board b : boardList){
+            log.warn(b.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("특정 제목 게시글 조회 테스트")
+    public void findByTitleTest(){
+        this.createBoardTest();
+        Optional<Board> boardList = boardRepository.findByTitleContaining("게시글4");
+//        Board board = boardRepository
+//                .findByTitleContaining("게시글7")
+//                .orElseThrow(()-> new RuntimeException("해당 게시글이 존재하지 않습니다."));
+        log.warn(boardList.toString());
+    }
+
+    @Test
+    @DisplayName("특정 내용 게시글 조회 테스트")
+    public void findByContentTest(){
+        this.createBoardTest();
+        List<Board> boardList = boardRepository.findByContentContaining("내용3");
+        log.warn(boardList.toString());
+    }
 
 }
