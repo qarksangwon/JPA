9d70687a2bda9e83fa7e67ea9ac43f6c09e775ca
qarksangdol <0629psw@gmail.com>
2024-06-14 10:22:38 +0900

    Swagger JWT Authorize 추가:

--- src/main/java/com/spb/total/config/SwaggerConfiguration.java
+++ src/main/java/com/spb/total/config/SwaggerConfiguration.java
@@ -7,15 +7,22 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Slf4j
 @Configuration
 @EnableSwagger2
 public class SwaggerConfiguration {
-    
+
     @Bean
     public Docket api() {
         log.info("Swagger api() 호출");
@@ -24,9 +31,11 @@ public class SwaggerConfiguration {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.spb.total"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .securitySchemes(Arrays.asList(apiKey()))
+                .securityContexts(Arrays.asList(securityContext()));
     }
-    
+
     private ApiInfo apiInfo(){
         log.info("Swagger apiInfo() 호출");
         return new ApiInfoBuilder()
@@ -34,5 +43,22 @@ public class SwaggerConfiguration {
                 .description("설명부")
                 .version("1.0.0")
                 .build();
+    }
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.any())
+                .build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
     }
 }
